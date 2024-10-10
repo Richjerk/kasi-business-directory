c4e833baebd7d7d7102b6f976bba8b89d255aee5
Richjerk <puseletso55@gmail.com>
2024-10-10 10:59:41 +0200

    Add functions directory and example function

---
+++ netlify/functions/hello.js
@@ -0,0 +1,7 @@
+// Create netlify/functions/hello.js
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, world!" })
+  };
+};
